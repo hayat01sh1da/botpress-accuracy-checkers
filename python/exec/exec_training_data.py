f912174c7dab696eb81ef092597d1bec3b8c5bbe
Hayato Ishida <bin.lhkty.gtthj.o.zs+github@gmail.com>
2024-10-19 22:10:05 +0900

    Delete __pycache__/ after execution

--- python/exec/exec_training_data.py
+++ python/exec/exec_training_data.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import shutil
+import glob
 sys.path.append('./src')
 sys.path.append('./src/lib')
 from training_data import TrainingData
@@ -10,3 +12,8 @@ dirname           = os.path.join('..', 'json')
 training_data = TrainingData(csv_training_data)
 training_data.export(dirname)
 print("--- Successfully exported JSON training data under {dirname}/ ---".format(dirname = dirname))
+
+pycaches = glob.glob(os.path.join('.', '**', '__pycache__'), recursive = True)
+for pycache in pycaches:
+    if os.path.isdir(pycache):
+        shutil.rmtree(pycache)
