265b24b7ab3211463e3a2e182c80162109a1fb67
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2025-02-08 23:19:38 +0900

    Specify unit tests to run

--- python/run_unittests.sh
+++ python/run_unittests.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-for unittest in $(find . -name test_*.py -type f)
+unittests=(./test/test_accuracy_check.py ./test/test_training_data.py ./test/lib/test_chart_drawer.py)
+for unittest in ${unittests[@]}
 do
   python $unittest
 done
