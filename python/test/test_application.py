01f7c6b0241df02a90033db1651498393fe3d9a6
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2026-04-12 01:13:47 +0900

    Introduce type checking to existing Python files

--- python/test/test_application.py
+++ python/test/test_application.py
@@ -4,12 +4,12 @@ import shutil
 import glob
 
 class TestApplication(unittest.TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         self.dirname  = os.path.join('test', 'tmp')
         self.pycaches = glob.glob(os.path.join('.', '**', '__pycache__'), recursive = True)
         os.makedirs(self.dirname, exist_ok = True)
 
-    def tearDown(self):
+    def tearDown(self) -> None:
         if os.path.exists(self.dirname):
             shutil.rmtree(self.dirname)
         for pycache in self.pycaches:
