5f5513db8b894b28431129df8bb756653226d31d
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2026-05-18 06:49:15 +0900

    Resolve violations against RUby convention reported RuboCop
    ```
    Offenses:
    
    spec/lib/csv_chart_drawer_spec.rb:8:28: C: [Corrected] Rails/FilePath: Prefer Rails.root.join('path/to').
      let(:csv_path)         { Rails.root.join('csv', 'test_data.csv') }
                               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    spec/lib/csv_chart_drawer_spec.rb:8:44: C: [Corrected] Style/StringLiterals: Prefer single-quoted strings when you don't need string interpolation or special symbols.
      let(:csv_path)         { Rails.root.join("csv/test_data.csv") }
                                               ^^^^^^^^^^^^^^^^^^^
    spec/lib/csv_chart_drawer_spec.rb:9:28: C: [Corrected] Rails/FilePath: Prefer Rails.root.join('path/to').
      let(:json_path)        { Rails.root.join('json', 'res_bodies.json') }
                               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    spec/lib/csv_chart_drawer_spec.rb:9:44: C: [Corrected] Style/StringLiterals: Prefer single-quoted strings when you don't need string interpolation or special symbols.
      let(:json_path)        { Rails.root.join("json/res_bodies.json") }
                                               ^^^^^^^^^^^^^^^^^^^^^^
    spec/lib/csv_chart_drawer_spec.rb:14:52: C: [Corrected] Performance/RedundantSplitRegexpArgument: Use string as argument instead of regexp.
        expect(test_data.length).to eq(csv_chart.split(/\n/).length)
                                                       ^^^^
    spec/lib/csv_chart_drawer_spec.rb:14:52: C: [Corrected] Style/RedundantRegexpArgument: Use string "\n" as argument instead of regexp /\n/.
        expect(test_data.length).to eq(csv_chart.split(/\n/).length)
                                                       ^^^^
    
    46 files inspected, 6 offenses detected, 6 offenses corrected
    ```

--- ruby-on-rails/spec/lib/csv_chart_drawer_spec.rb
+++ ruby-on-rails/spec/lib/csv_chart_drawer_spec.rb
@@ -5,12 +5,12 @@ require 'rails_helper'
 RSpec.describe CsvChartDrawer do
   let(:csv_chart_drawer) { described_class.new(csv_path, res_bodies) }
   let(:csv_chart)        { csv_chart_drawer.csv }
-  let(:csv_path)         { Rails.root.join('csv', 'test_data.csv') }
-  let(:json_path)        { Rails.root.join('json', 'res_bodies.json') }
+  let(:csv_path)         { Rails.root.join('csv/test_data.csv') }
+  let(:json_path)        { Rails.root.join('json/res_bodies.json') }
   let(:test_data)        { CSV.read(csv_path) }
   let(:res_bodies)       { File.read(json_path).then { |json| JSON.parse(json) } }
 
   it 'returns the same number of rows in the CSV chart as that in the test data' do
-    expect(test_data.length).to eq(csv_chart.split(/\n/).length)
+    expect(test_data.length).to eq(csv_chart.split("\n").length)
   end
 end
